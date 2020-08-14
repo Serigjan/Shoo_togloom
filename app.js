d3e1c1b432ae0477c1ef5e376f16a9b3af2b710f
Seku <sekujan03@gmail.com>
2020-08-14 11:08:57 +0800

    hadgaldag bolgono.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // togloogchiin eelj
-var activePlayer = 1;
+var activePlayer = 0;
 
 // onoo hadgalah 
 var scores = [0, 0];
@@ -8,12 +8,7 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Shooni ali talaar buusaniig ...
-var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-
- window.document.querySelector("#score-0").textContent = diceNumber;
-
- document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
 
 // program ehlehed
 
@@ -23,7 +18,7 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 
 
-
+// shoog shiideh eventListener
 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
@@ -32,4 +27,25 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
   diceDom.style.display = "block";
   diceDom.src = "dice-" + diceNumber + ".png";
-});
+
+
+// Togloogchiin eeljin onoog uurchilnu.
+
+  if(diceNumber !==1)
+  {
+    roundScore=roundScore+diceNumber;
+    document.getElementById("current-"+ activePlayer).textContent =roundScore;  
+  }
+  else
+  {
+    roundScore=0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    diceDom.style.display = "none";
+  }
+  });
