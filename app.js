1f051ed429d7e82c071095ee79970647af04341a
Seku <sekujan03@gmail.com>
2020-08-14 10:25:41 +0800

    Shoog shiiddeg bolgoh

--- app.js
+++ app.js
@@ -8,22 +8,21 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Shooni ali talaar buusaniig ...
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 
- window.document.querySelector("#score-0").textContent = dice;
+ window.document.querySelector("#score-0").textContent = diceNumber;
 
  document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
 
 // program ehlehed
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
 
-document.querySelector(".dice").style.display = "none";
 
 
 var diceDom = document.querySelector(".dice");
